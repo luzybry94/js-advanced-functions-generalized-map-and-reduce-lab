0a367ecbc96c2e880dbbd693a2b8098b97ac89bc
luzybry94 <luzybry94@gmail.com>
2021-03-17 14:01:50 -0400

    commit tree 98ec3d2a57a42373a6ab74e9cfcd57fb91524f60

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(arr, func) {
+    let newArr = []
+  
+    for (let i = 0; i < arr.length; i++) {
+      let el = arr[i]
+      newArr.push(func(el))
+    }
+  
+    return newArr;
+  }
+
+  function reduce(arr, func, start){
+    let r = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < arr.length; i++) {
+      r = func(arr[i], r)
+    }
+  
+    return r;
+  }
